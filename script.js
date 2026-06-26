46abf0ec352ea6a5e79847d96d1880ea5674b798
R3BORN2065 <r3born2065@outlook.com>
2026-06-26 15:14:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-const btn=document.getElementById('topBtn');window.addEventListener('scroll',()=>{btn.style.display=window.scrollY>800?'block':'none'});btn.addEventListener('click',()=>scrollTo({top:0,behavior:'smooth'}));
+const topBtn = document.getElementById('topBtn');
+window.addEventListener('scroll', () => { topBtn.style.display = window.scrollY > 600 ? 'block' : 'none'; });
+topBtn.addEventListener('click', () => window.scrollTo({ top: 0, behavior: 'smooth' }));
